52a5a025650376cbec80e69c95900a98dabb33b9
kotoshin <yspm1127@gmail.com>
2024-08-03 23:49:56 +0900

    レッスン14　課題1

---
+++ src/main/java/com/techacademy/KadaiFirstController.java
@@ -0,0 +1,59 @@
+package com.techacademy;
+
+import java.util.Calendar;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KadaiFirstController {
+
+    @GetMapping("/dayofweek/{val1}")
+    public String index(@PathVariable String val1) {
+        // 20240805
+        String nen = val1.substring(0, 4);
+        String tuki = val1.substring(4, 6);
+        String day = val1.substring(6, 8);
+
+        Calendar cl = Calendar.getInstance();
+        cl.set(Calendar.YEAR, Integer.parseInt(nen));
+        cl.set(Calendar.MONTH, Integer.parseInt(tuki) - 1);
+        cl.set(Calendar.DATE, Integer.parseInt(day));
+
+        String youbi = "";
+        switch (cl.get(Calendar.DAY_OF_WEEK)) {
+        case Calendar.SUNDAY: // Calendar.SUNDAY:1
+            // 日曜日
+            youbi = "Sunday";
+            break;
+        case Calendar.MONDAY: // Calendar.MONDAY:2
+            // 月曜日
+            youbi = "Monday";
+            break;
+        case Calendar.TUESDAY: // Calendar.TUESDAY:3
+            // 火曜日
+            youbi = "Tuesday";
+            break;
+        case Calendar.WEDNESDAY: // Calendar.WEDNESDAY:4
+            // 水曜日
+            youbi = "Wednesday";
+            break;
+        case Calendar.THURSDAY: // Calendar.THURSDAY:5
+            // 木曜日
+            youbi = "Thursday";
+            break;
+        case Calendar.FRIDAY: // Calendar.FRIDAY:6
+            // 金曜日
+            youbi = "Friday";
+            break;
+        case Calendar.SATURDAY: // Calendar.SATURDAY:7
+            // 土曜日
+            youbi = "Saturday";
+            break;
+        }
+
+        return youbi;
+    }
+
+}
---
+++ src/main/java/com/techacademy/KadaiFirstSpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.techacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class KadaiFirstSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KadaiFirstSpringBootApplication.class, args);
+	}
+
+	
+}
